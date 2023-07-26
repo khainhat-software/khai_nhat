b750a325c668b4dbd28024df776e59ddb82d60d6
khainhat-software <66371009+khainhat-software@users.noreply.github.com>
2023-07-26 14:02:48 +0700

    All commits

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+	strings: ["Leonardo.", "Michelangelo.","Donatello.", "Raphael."],
+	typeSpeed: 20,
+	backSpeed: 50,
+	loop:true,
+});
+
+$('.counter').countUp();
